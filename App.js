bc099a0e441b84a5888bef5d596fde3048509008
dhaneshKatre <dhaneshkatre@gmail.com>
2018-02-24 14:52:41 +0530

    Initial Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View } from 'react-native';
+import ChatUI from './src/ChatUI';
+
+export default class App extends React.Component {
+  render() {return <ChatUI />}
+}
